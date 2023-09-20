ead8013604b9a069834cd932e55debc96152bf45
zealotchen <zealotchen@tencent.com>
2023-09-20 19:35:16 +0800

    feat(vue): not update node when add/remove listener

--- driver/js/packages/hippy-vue/src/renderer/native/index.js
+++ driver/js/packages/hippy-vue/src/renderer/native/index.js
@@ -177,6 +177,7 @@ function endBatch(app) {
           break;
         case NODE_OPERATION_TYPES.updateEvent:
           handleEventListeners(chunk.eventNodes, sceneBuilder);
+          break;
         default:
       }
     });
